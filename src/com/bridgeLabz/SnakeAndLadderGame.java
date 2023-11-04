3999fcb91297a27f3a563ffe81bee24f7e83b761
smsmaurya <sumeshmaurya786@gmail.com>
2023-11-04 15:45:44 +0530

    Welcome message

---
+++ src/com/bridgeLabz/SnakeAndLadderGame.java
@@ -0,0 +1,8 @@
+package com.bridgeLabz;
+
+public class SnakeAndLadderGame {
+
+    public static void main(String[] args) {
+        System.out.println("  Welcome in Snake and Ladder game.");
+    }
+}
