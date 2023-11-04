a6a0e4fcb85c3891d46076edb2d5691c947ff9f3
smsmaurya <sumeshmaurya786@gmail.com>
2023-11-04 17:29:44 +0530

    Added :- Repeat till the player reaches the winning position

--- src/com/bridgeLabz/SnakeAndLadderGame.java
+++ src/com/bridgeLabz/SnakeAndLadderGame.java
@@ -1,6 +1,6 @@
 package com.bridgeLabz;
 
-import java.util.Random;
+//import java.util.Random;
 
 public class SnakeAndLadderGame {
 
@@ -9,34 +9,38 @@ public class SnakeAndLadderGame {
         int playerPosition = 0;
         System.out.println("The starting player position is : "+playerPosition);
 
-        Random random = new Random(); // create object of random class
+        // Random random = new Random(); // create object of random class
 
-        // player rolls the dice
-        // int rollsDice = random.nextInt(6)+1;
-        int rollsDice = (int)(Math.random()*10)%6 +1;
-        System.out.println("Player rolls the dice and getting number on dice is : "+rollsDice);
+        while (playerPosition<100){
+            // player rolls the dice
+            // int rollsDice = random.nextInt(6)+1;
+            int rollsDice = (int)(Math.random()*10)%6 +1;
+            System.out.println("Player rolls the dice and getting number on dice is : "+rollsDice);
 
-        // automatic generate situation to decide next step after rolling dice
-        // int situation = random.nextInt(3);
-        int situation = (int)(Math.random()*10)%3;
+            // automatic generate situation to decide next step after rolling dice
+            // int situation = random.nextInt(3);
+            int situation = (int)(Math.random()*10)%3;
 
-        //
-        switch (situation){
-            case 0:          // No play situation
-                System.out.println("Player 'no Play' situation.");
-                System.out.println("The current position of player is :"+playerPosition);
-                break;
+            //
+            switch (situation){
+                case 0:          // No play situation
+                    System.out.println("Player 'no Play' situation.");
+                    System.out.println("The current position of player is :"+playerPosition);
+                    break;
 
-            case 1:          // ladder situation
-                playerPosition += rollsDice;
-                System.out.println("After ladder situation,\nplayer position is:"+playerPosition);
-                break;
+                case 1:          // ladder situation
+                    playerPosition += rollsDice;
+                    System.out.println("After ladder situation,\nplayer position is:"+playerPosition);
+                    break;
 
-            case 2:          // Snakebite situation
-                playerPosition -= rollsDice;
-                System.out.println("After snake bite situation,\nplayer position is :"+playerPosition);
-                break;
-
+                case 2:          // Snakebite situation
+                    playerPosition -= rollsDice;
+                    if(playerPosition<0){
+                        playerPosition = 0;
+                    }
+                    System.out.println("After snake bite situation,\nplayer position is :"+playerPosition);
+                    break;
+            }
         }
     }
 }
