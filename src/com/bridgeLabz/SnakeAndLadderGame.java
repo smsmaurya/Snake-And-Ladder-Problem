6492b68d5d81d5c754feff2ba9724e35b7f15632
smsmaurya <sumeshmaurya786@gmail.com>
2023-11-04 17:07:34 +0530

    Added :- Check situation then move next step

--- src/com/bridgeLabz/SnakeAndLadderGame.java
+++ src/com/bridgeLabz/SnakeAndLadderGame.java
@@ -1,14 +1,42 @@
 package com.bridgeLabz;
 
+import java.util.Random;
+
 public class SnakeAndLadderGame {
 
     public static void main(String[] args) {
-        System.out.println(" Start Snake and ladder game");
-        int playerPosition = 0;
-        System.out.println("The player position is : "+playerPosition);
 
+        int playerPosition = 0;
+        System.out.println("The starting player position is : "+playerPosition);
+
+        Random random = new Random(); // create object of random class
+
+        // player rolls the dice
+        // int rollsDice = random.nextInt(6)+1;
         int rollsDice = (int)(Math.random()*10)%6 +1;
         System.out.println("Player rolls the dice and getting number on dice is : "+rollsDice);
 
+        // automatic generate situation to decide next step after rolling dice
+        // int situation = random.nextInt(3);
+        int situation = (int)(Math.random()*10)%3;
+
+        //
+        switch (situation){
+            case 0:          // No play situation
+                System.out.println("Player 'no Play' situation.");
+                System.out.println("The current position of player is :"+playerPosition);
+                break;
+
+            case 1:          // ladder situation
+                playerPosition += rollsDice;
+                System.out.println("After ladder situation,\nplayer position is:"+playerPosition);
+                break;
+
+            case 2:          // Snakebite situation
+                playerPosition -= rollsDice;
+                System.out.println("After snake bite situation,\nplayer position is :"+playerPosition);
+                break;
+
+        }
     }
 }
